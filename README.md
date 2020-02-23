e93188ed36da0ff001dc8c516fb438479d45e224
AmandaWhiteTX <54691794+AmandaWhiteTX@users.noreply.github.com>
2020-02-22 19:14:48 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Click Game
+
+This game requires the user to click each image while not clicking the same image twice. If the user clicks an image a second time they lose and their score resets. 
+
+Upon losing he screen shakes to signify that the game is over while tacking their current score and showing their high score.
+
+![game](./public/images/clicky.gif)
---
+++ build/precache-manifest.4d7dbb887dcbbc4051293a3a6c49716a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a733b25a883450f02867be3013c48d62",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "dcde3f90364a24f7e590",
+    "url": "/clickyGame/static/css/main.ab91a8ef.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "dcde3f90364a24f7e590",
+    "url": "/clickyGame/static/js/main.4836b68a.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
