f36251bd7c75a89d613237b7f36678c34f6d982f
AmandaWhiteTX <54691794+AmandaWhiteTX@users.noreply.github.com>
2020-02-23 22:40:04 -0600

    Updates

--- precache-manifest.f23ffa715d2b296a2db3df8407c530c9.js
+++ precache-manifest.f23ffa715d2b296a2db3df8407c530c9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98da8e3c8071f8b8adebc8c7cda9f2bd",
+    "revision": "796a3e61f9e249d765dd7f619e9751e6",
     "url": "/clickygame/index.html"
   },
   {
-    "revision": "488d897b1fa2bb50c478",
-    "url": "/clickygame/static/css/main.8a32d97d.chunk.css"
+    "revision": "90a3d5689b0cb9e17afb",
+    "url": "/clickygame/static/css/main.8713763e.chunk.css"
   },
   {
     "revision": "45124f853ba07aa965c0",
     "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
   },
   {
-    "revision": "488d897b1fa2bb50c478",
-    "url": "/clickygame/static/js/main.0340eb0a.chunk.js"
+    "revision": "90a3d5689b0cb9e17afb",
+    "url": "/clickygame/static/js/main.01499966.chunk.js"
   },
   {
     "revision": "0f0146524616eea72d1b",
